c0e2647e1808ed8a671bf047a703e168d08a584d
oddfeed <144010979+oddfeed@users.noreply.github.com>
2024-01-03 19:19:13 +0000

    Web friendly

---
+++ index.md
@@ -0,0 +1,4 @@
+# Masterlist
+- [index](https://oddfeed.github.io/bookclub/index.md)
+- [README](https://oddfeed.github.io/bookclub/README.md)
+- [timenergy](https://oddfeed.github.io/bookclub/timenergy.md)
---
+++ slides.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm index.md
+# File to write
+output_file="index.md"
+echo "# Masterlist"  >> "$output_file"
+# Loop through each HTML file in the current directory
+for file in *.md; do
+    # Extract the filename without extension
+    filename="${file%.*}"
+
+    # Append the formatted line to the output file
+    echo "- [$filename](https://oddfeed.github.io/bookclub/$file)" >> "$output_file"
+done
+
